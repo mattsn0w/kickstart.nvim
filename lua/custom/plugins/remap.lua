3d5e7eafb5b5fc10d8412c437c73163c2b260971
Matt Snow <matt@mattsn0w.com>
2025-07-19 22:45:16 -0600

    great keymaps from Primeagen

---
+++ lua/custom/plugins/remap.lua
@@ -0,0 +1,9 @@
+
+
+# Key remaps from https://github.com/ThePrimeagen/init.lua/blob/249f3b14cc517202c80c6babd0f9ec548351ec71/lua/theprimeagen/remap.lua
+#
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
